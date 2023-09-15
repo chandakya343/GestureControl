af2ced189db235b4ebedf6d11627ba0fe1cfcbc3
siddharthiitian <siddharth.prakash.bme21@itbhu.ac.in>
2023-09-16 01:46:23 +0530

    first and thumbs up detected

--- model.py
+++ model.py
@@ -23,15 +23,27 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            
+            thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+            thumb_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_MCP]
+            index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+            # print("Thumb Tip:", thumb_tip)
+            # print("Thumb MCP:", thumb_mcp)
+            # Detecting thumbs-up gesture
+            if thumb_tip.y < thumb_mcp.y:
+                cv2.putText(frame, "Thumbs Up!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            if index_finger_tip.y > thumb_mcp.y:
+                cv2.putText(frame, "Fist!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                # print("Thumbs Up")
     # Display the resulting frame
     cv2.imshow('Gesture Controlled PC Assistant', frame)
 
     # Press 'q' to exit the loop and close the window
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
 
 # Release the webcam and destroy all OpenCV windows
 hands.close()
 cap.release()
 cv2.destroyAllWindows()
+# Draw hand landmarks and recognize gestures
